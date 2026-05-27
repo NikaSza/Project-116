9714eba77a82d6d99b98373d471c39a633950c14
NikaSza <szafranskanika@gmail.com>
2026-05-27 11:23:47 +0200

    Fix tree selection and update bins slider settings

--- nika_read1.py
+++ nika_read1.py
@@ -10,11 +10,17 @@ root_file_path = r"C:\Users\nika\OneDrive\Dokumenty\python_practical_2026\Projec
 file = uproot.open(root_file_path)
 
 print(file.keys())
-keys = file.keys()[0]  
 
+keys = file.keys()[1]   
+tree = file[keys]
+
+print(tree.keys())
 
 def plot(variable, bins=100, x_min=None, x_max=None):
     data = tree[variable].array(library="np")
+
+    # Remove invalid values
+    data = data[np.isfinite(data)]
 
     plt.figure(figsize=(8, 5))
     plt.hist(data, bins=bins, range=(x_min, x_max), histtype="step")
@@ -26,12 +32,16 @@ variable_dropdown = widgets.Dropdown(
     options=variables,
     description="Variable:"
 )
+
 bins_slider = widgets.IntSlider(
     value=100,
-    min=10,
+    min=1,
     max=300,
-    step=10,
+    step=1,
     description="Bins:"
 )
+
 widgets.interact(plot, variable=variable_dropdown, bins=bins_slider, x_min=widgets.FloatText(value=None, description="x min"), x_max=widgets.FloatText(value=None, description="x max")
 )
+
+print(tree.keys())
