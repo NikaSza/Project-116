381fb7971c186bc913e780079b97fc08e94bf211
NikaSza <szafranskanika@gmail.com>
2026-05-28 11:58:37 +0200

    Added is it muon conditions

--- nika_day2code.py
+++ nika_day2code.py
@@ -19,30 +19,41 @@ all_branches = tree.keys()
 variables = list(tree.keys())
 
 ###Conditions###
-#Add new functions and limits here 
-
+#Add new functions and limits here then change only the combine_masks function to add new cuts to the final plot
+#rest should be unchanged
 
 def kaon_probability_mask(k_threshold=0.85): # change the treshold if you want more or less stict cuts
     kplus = tree["Kplus_ProbNNk"].array(library="np")
     kplus_mask = np.isfinite(kplus) & (kplus > k_threshold)
     return kplus_mask
 
-#example of condition to be changed later/ add similar functions below
 def pminus_probability_mask(pmin_threshold=0.1): # change the treshold if you want more or less stict cuts
     pminus = tree["piminus_ProbNNpi"].array(library="np")
     pminus_mask = np.isfinite(pminus) & (pminus > pmin_threshold)
     return pminus_mask
+
+#checking if it is not a muon
+def muplus_isMuon_mask():
+    muplus_isMuon = tree["muplus_isMuon"].array(library="np")
+    muplus_mask = np.isfinite(muplus_isMuon) & (muplus_isMuon != 0)
+    return muplus_mask
+def muminus_isMuon_mask():
+    muminus_isMuon = tree["muminus_isMuon"].array(library="np")
+    muminus_mask = np.isfinite(muminus_isMuon) & (muminus_isMuon != 0)
+    return muminus_mask
+
 
 #combine conditions -add new masks to variable list here
 def combine_masks(k_threshold=0.85, pmin_threshold=0.1):
     return (
         kaon_probability_mask(k_threshold)
         & pminus_probability_mask(pmin_threshold)
+        & muplus_isMuon_mask()
+        & muminus_isMuon_mask()
     )
 
 
 ###Plotting two graphs next to each other
-
 def plot_with_cuts(variable, k_threshold=0.85, pmin_threshold=0.1, bins=100, x_min=None, x_max=10000):
 
     # Load raw data (never modify this)
@@ -52,6 +63,7 @@ def plot_with_cuts(variable, k_threshold=0.85, pmin_threshold=0.1, bins=100, x_m
     if variable == "B0_PT":
         data_plot = data / 1000.0
         xlabel = "B0_PT [GeV/c]"
+        plt.yscale("log")
     else:
         data_plot = data
         xlabel = variable
@@ -103,7 +115,7 @@ k_threshold_slider = widgets.FloatSlider(
     step=0.01,
     description="K+ threshold:"
 )
-pm_threshold_slider = widgets.FloatSlider(
+pmin_threshold_slider = widgets.FloatSlider(
     value=0.1,
     min=0,
     max=1,
@@ -114,7 +126,7 @@ widgets.interact(
     plot_with_cuts,
     variable=variable_dropdown,
     k_threshold=k_threshold_slider,
-    pmin_threshold=pm_threshold_slider,
+    pmin_threshold=pmin_threshold_slider,
     bins=bins_slider,
     x_min=widgets.FloatText(value=None, description="x min"),
     x_max=widgets.FloatText(value=None, description="x max")
